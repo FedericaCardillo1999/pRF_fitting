ef81e0cd31d243727a8e25e2bfb749a67d5e4066
Federica Cardillo <163155072+FedericaCardillo1999@users.noreply.github.com>
2025-06-11 10:55:05 +0200

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,34 +1,58 @@
-# Denoising anatomical 
-# master -m 08 -s 01 -n 01 --ow
+#!/bin/bash
+#SBATCH --job-name=upsampling
+#SBATCH --time=03:00:00
+#SBATCH --partition=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=25    
+#SBATCH --profile=task
+#SBATCH --mem=50GB
+#SBATCH --output=upsampling.out
+export PATH=/usr/bin:/bin:$PATH
 
-# Denoising functional 
-# master -m 10 -s 01 -n 01 --ow
+source $HOME/venvs/preproc/bin/activate
+source ~/.bash_profile
+
+input="$1"
+input="${input#sub-}"
+subject_id="sub-$input"
+
+# Functional denoising 
+# master -m 10 -s "$input" -n 01 --ow
+# master -m 10 -s "$input" -n 02 --ow
 
 # Motion Correction 
-# source /home2/p315561/programs/cflaminar/shell/motion_correction/spm_moco.sh subject task nruns
+# source /home2/p315561/programs/cflaminar/shell/motion_correction/spm_moco.sh "$input" RET 
+# source /home2/p315561/programs/cflaminar/shell/motion_correction/spm_moco.sh "$input" RET2
+# source /home2/p315561/programs/cflaminar/shell/motion_correction/spm_moco.sh "$input" RestingState
 
-# Coregistration 
-source /home2/p315561/programs/cflaminar/shell/coregistration/coreg_T2.sh subj ses
+# Coregistration
+# source /home2/p315561/programs/cflaminar/shell/coregistration/job_applyTransforms.sh "$input" RET
+# source /home2/p315561/programs/cflaminar/shell/coregistration/job_applyTransforms.sh "$input" RET2
+source /home2/p315561/programs/cflaminar/shell/coregistration/job_applyTransformsEGRET.sh "$input" RestingState
 
+# Freesurfer
+# master -m 14 -s 01 -n -01
 
+# fMRIprep
+# master -m 15 -s 01 -n -01
 
-# Freesurfer 
-# master -m 14 -s 01 -n 01 --ow
+# Benson atlas and Bayesian Benson atlas
 
-# Pycortex
-
-# Atlas 
-# python /home2/p315561/programs/cflaminar/shell/atlas/bayesian_benson.py subject denoising
-# source /home2/p315561/programs/cflaminar/shell/atlas/standard_benson.sh subject
-
-# Resampling 
-# source /home2/p315561/programs/cflaminar/shell/resampling/resampling.sh subject task
+# Resampling
+# source /home2/p315561/programs/cflaminar/shell/resampling/resampling.sh "$input" RET 
+# source /home2/p315561/programs/cflaminar/shell/resampling/resampling.sh "$input" RET2
 
 # Filtering
-# python /home2/p315561/programs/cflaminar/pRF_fitting/filtering.py subject task
+# python /home2/p315561/programs/cflaminar/pRF_fitting/filtering.py "$input" RET 
+# python /home2/p315561/programs/cflaminar/pRF_fitting/filtering.py "$input" RET2
 
-# pRF fitting 
-# python /home2/p315561/programs/cflaminar/pRF_fitting/main_PRFs.py.py subject denoising depth atlas ncores task
 
-# Visual Field Maps 
-# python /home2/p315561/programs/cflaminar/pRF_fitting/visual_field_maps.py subject task
+# pRF mapping
+# python /home2/p315561/programs/cflaminar/pRF_fitting/fit_PRFs.py "$input" nordic GM benson 45 RET
+# python /home2/p315561/programs/cflaminar/pRF_fitting/fit_PRFs.py "$input" nordic_sm4 GM benson 45 RET
+# python /home2/p315561/programs/cflaminar/pRF_fitting/fit_PRFs.py "$input" nordic GM manual 45 RET
+# python /home2/p315561/programs/cflaminar/pRF_fitting/fit_PRFs.py "$input" nordic_sm4 GM manual 45 RET
+
+
+# CF modeling 
+# python /scratch/hb-EGRET-AAA/habrok2.py "$input" 
